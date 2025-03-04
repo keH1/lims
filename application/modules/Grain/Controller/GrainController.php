3aaf3d4651219113126d4d73e91d86574ed26afb
Родионов Роман <r.roman@niistrom.pro>
2025-03-04 12:07:46 +0500

    task/LOKI-1023/Залил зерновые составы

---
+++ application/modules/Grain/Controller/GrainController.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Класс контроллер для Зернового состава
+ * Class GrainController
+ */
+
+class GrainController extends Controller
+{
+    public function card(int $grainListID)
+    {
+        // if (empty($dealId)) {
+        //     $this->redirect('/request/list/');
+        // }
+
+        /** @var Grain $grain*/
+        $grain = $this->model('Grain');
+
+        $this->data['title'] = "Зерновой состав";
+
+        $this->data['grain_list_id'] = $grainListID;
+        $this->data['grain_list_gost'] = $grain->getGrainGostList($grainListID);
+        $this->data['grain_seave_size'] = $grain->getGrainSeaveSize();
+        $this->data['grain'] = $grain->getGrainSeaveValues($grainListID);
+
+        // echo '<pre>';
+        // print_r($this->data['grain_seave_size']);
+        // die;
+
+        $this->addCSS("/assets/plugins/select2/dist/css/select2.min.css");
+        $this->addCSS("/assets/plugins/select2/dist/css/select2-bootstrap-5-theme.min.css");
+
+        $this->addJs('/assets/plugins/select2/dist/js/select2.min.js');
+
+        $this->addJs('/assets/js/grain/grain.js');
+        $this->view('card');
+    }
+
+    public function updateGrainList(int $grainListID)
+    {
+        /** @var Grain $grain*/
+        $grain = $this->model('Grain');
+
+        $grain->update($grainListID, $_POST);
+
+        $this->redirect('/grain/card/' . $grainListID);
+    }
+
+    public function list()
+    {
+        $this->data['title'] = 'Журнал зерновых составов';
+
+        /** @var Request $request */
+        $request = $this->model('Request');
+        /** @var Grain $grain */
+        $grain = $this->model('Grain');
+
+        $this->data['date_start'] = $request->getDateStart();
+
+        // echo '<pre>';
+        // print_r($this->data['date_start']);
+        // die;
+
+        $this->addCSS("/assets/plugins/DataTables/datatables.min.css");
+        $this->addCSS("/assets/plugins/DataTables/ColReorder-1.5.5/css/colReorder.dataTables.min.css");
+        $this->addCSS("/assets/plugins/DataTables/Buttons-2.0.1/css/buttons.dataTables.min.css");
+
+        $this->addJS("/assets/plugins/DataTables/DataTables-1.11.3/js/jquery.dataTables.min.js");
+        $this->addJS("/assets/plugins/DataTables/ColReorder-1.5.5/js/dataTables.colReorder.min.js");
+        $this->addJS("/assets/plugins/DataTables/Buttons-2.0.1/js/dataTables.buttons.min.js");
+        $this->addJS("/assets/plugins/DataTables/Buttons-2.0.1/js/buttons.colVis.min.js");
+        $this->addJS("/assets/plugins/DataTables/Buttons-2.0.1/js/buttons.print.min.js");
+        $this->addJS("/assets/plugins/DataTables/Buttons-2.0.1/js/buttons.html5.min.js");
+        $this->addJS("/assets/plugins/DataTables/JSZip-2.5.0/jszip.min.js");
+        $this->addJS("/assets/plugins/DataTables/dataRender/ellipsis.js");
+        $this->addJS("/assets/plugins/DataTables/dataRender/intl.js");
+        $this->addJS("/assets/plugins/DataTables/FixedHeader-3.2.0/js/dataTables.fixedHeader.min.js");
+
+        $this->addJs("/assets/js/grain/grain-list.js");
+
+        $this->view('list');
+    }
+
+    public function getListProcessingAjax()
+    {
+        global $APPLICATION;
+        $APPLICATION->RestartBuffer();
+
+        /** @var Grain $grain */
+        $grain = $this->model('Grain');
+
+        $filter = [
+            'paginate' => [
+                'length'    => $_POST['length'],  // кол-во строк на страницу
+                'start'      => $_POST['start'],  // текущая страница
+            ],
+            'search' => [],
+            'order' => []
+        ];
+
+        foreach ($_POST['columns'] as $column) {
+            if ( !empty($column['search']['value']) ) {
+                $filter['search'][$column['data']] = $column['search']['value'];
+            }
+        }
+
+        if ( isset($_POST['order']) && !empty($_POST['columns']) ) {
+            $filter['order']['by']  = $_POST['columns'][$_POST['order'][0]['column']]['data'];
+            $filter['order']['dir'] = $_POST['order'][0]['dir'];
+        }
+
+        $data = $grain->getDataToJournalGrain($filter);
+
+        $recordsTotal = $data['recordsTotal'];
+        $recordsFiltered = $data['recordsFiltered'];
+
+        unset($data['recordsTotal']);
+        unset($data['recordsFiltered']);
+
+        $jsonData = [
+            "draw" => $_POST['draw'],
+            "recordsTotal" => $recordsTotal,
+            "recordsFiltered" => $recordsFiltered,
+            "data" => $data
+        ];
+
+        echo json_encode($jsonData, JSON_UNESCAPED_UNICODE);
+    }
+}
---
+++ application/modules/Grain/Model/Grain.php
@@ -0,0 +1,192 @@
+<?php
+
+/**
+ * Класс модель для Зернового состава
+ * Class Grain
+ */
+class Grain extends Model
+{
+    const SEAVE = [
+        0 => [
+            "seave_main_type" => "round",
+            "seave_type" => [
+                0 => [
+                    "type_to_input" => 200,
+                    "type_to_title" => "Обечайка 200",
+                    "values" => [
+                        "0,05 (сетка)",  "0,063 (сетка)",     "0,071 (сетка)",    "0,1 (сетка)",
+                        "0,125 (сетка)", "0,140 (сетка)",     "0,160 (сетка)",    "0,250 (сетка)",
+                        "0,315 (сетка)", "0,5 (сетка)",       "0,63 (сетка)",     "0,7 (сетка)",
+                        "0,9 (сетка)",   "1,25 (перф.)", "2,5 (перф.)", "5,0 (перф.)"
+                    ]
+                ],
+                1 => [
+                    "type_to_input" => 300,
+                    "type_to_title" => "Обечайка 300",
+                    "values" => [
+                        "1,0 (сетка)",        "2,0 (сетка)", "3,0", "6,5", "7,5 (перф.)",
+                        "10,0 (перф.)",  "12,5 (перф.)",  "15,0 (перф.)",  "17,5 (перф.)",
+                        "18,75 (перф.)", "20,0 (перф.)",  "25,0 (перф.)",  "30,0 (перф.)",
+                        "40,0 (перф.)",  "50,0 (перф.)",  "55,0 (перф.)",  "60,0 (перф.)",
+                        "70,0 (перф.)",  "80,0 (перф.)",  "87,5 (перф.)",  "90,0 (перф.)",
+                        "95,0 (перф.)",  "100,0 (перф.)", "110,0 (перф.)", "120,0 (перф.)",
+                        "150,0 (перф.)"
+                    ]
+                ]
+            ]
+        ],
+        1 => [
+            "seave_main_type" => "square",
+            "seave_type" => [
+                0 => [
+                    "type_to_input" => 200,
+                    "type_to_title" => "Обечайка 200",
+                    "values" => [
+                        "1,0 (сетка)", "1,6", "2,0 (сетка)",       "2,8 (сетка)",       "4,0 (сетка)",
+                        "5,6 (перф.)",   "6,3 (перф.)",  "8,0 (сетка)",       "11,2 (перф.)",
+                        "16,0 (перф.)",  "22,4 (перф.)", "31,5 (перф.)", "45,0 (перф.)"
+                    ]
+                ],
+                1 => [
+                    "type_to_input" => 300,
+                    "type_to_title" => "Обечайка 300",
+                    "values" => [
+                        "2,8 (перф.)",  "4,0 (перф.)",   "5,6 (перф.)",  "6,3 (перф.)",
+                        "8,0 (перф.)",  "11,2 (перф.)",  "14,0 (перф.)", "16,0 (перф.)",
+                        "22,4 (перф.)", "31,5 (перф.)",  "45,0 (перф.)", "63,0 (перф.)",
+                        "90,0 (перф.)", "126,0 (перф.)", "180,0 (перф.)"
+                    ]
+                ]
+            ]
+        ],
+    ];
+
+    public function getGrainGostList(int $grainListID)
+    {
+        $result = [];
+        $ba_gost = $this->DB->Query("SELECT bg.ID AS gost_id,
+                                            bg.GOST,
+                                            bg.GOST_PUNKT,
+                                            bg.SPECIFICATION,
+                                            zr.ID AS grain_id
+                                     FROM ba_gost AS bg
+
+                                     LEFT JOIN ZERN AS zr
+                                     ON bg.ID = zr.GOST_ID
+
+                                     WHERE bg.NON_ACTUAL != 1
+        ");
+
+        while ($gost = $ba_gost->Fetch()) {
+            $result[] = [
+                "gost_id" => $gost['gost_id'],
+                "gost" => $gost['GOST'],
+                "gost_point" => $gost['GOST_PUNKT'],
+                "gost_specification" => $gost['SPECIFICATION'],
+                "grain_list_id" => $gost['grain_id']
+            ];
+        }
+
+        return $result;
+    }
+
+    public function getGrainSeaveSize()
+    {
+        return SELF::SEAVE;
+    }
+
+    public function getGrainSeaveValues(int $grainListID)
+    {
+        $result = [];
+        $query = $this->DB->Query("SELECT *
+                                   FROM ZERN
+                                   WHERE ID = " . $grainListID
+        )->Fetch();
+
+        $result = [
+            "name" => $query['NAME'],
+            "data" => json_decode($query['DATA'], true)
+        ];
+
+        return $result;
+    }
+
+    public function update(int $grainListID, array $post)
+    {
+        $data['NAME'] = $this->quoteStr($post['grain_list_name']);
+        $data['GOST_ID'] = isset($post['grain_list_gost']) ? $post['grain_list_gost'] : "";
+        $data['DATA'] =  $this->quoteStr(json_encode($post['grain'], JSON_UNESCAPED_UNICODE));
+
+        $this->DB->Update("ZERN", $data,
+                          "WHERE ID = " . $grainListID);
+    }
+
+    public function getDataToJournalGrain(array $filter = []): array
+    {
+        $where = "";
+        $limit = "";
+        $order = [
+            'by' => 'NAME',
+            'dir' => 'DESC'
+        ];
+
+        if ( !empty($filter) ) {
+            // работа с фильтрами
+            if ( !empty($filter['search']) ) {
+                if ( isset($filter['search']['material_name']) ) {
+                    $where .= "NAME LIKE '%{$filter['search']['material_name']}%' AND ";
+                }
+            }
+
+            // работа с сортировкой
+            if ( !empty($filter['order']) ) {
+                if ( $filter['order']['dir'] === 'asc' ) {
+                    $order['dir'] = 'ASC';
+                }
+            }
+
+            // работа с пагинацией
+            if ( isset($filter['paginate']) ) {
+                $offset = 0;
+                // количество строк на страницу
+                if ( isset($filter['paginate']['length']) && $filter['paginate']['length'] > 0 ) {
+                    $length = $filter['paginate']['length'];
+
+                    if ( isset($filter['paginate']['start']) && $filter['paginate']['start'] > 0 ) {
+                        $offset = $filter['paginate']['start'];
+                    }
+                    $limit = "LIMIT {$offset}, {$length}";
+                }
+            }
+        }
+        $where .= "1 ";
+
+        $result = [];
+
+        $data = $this->DB->Query("SELECT `ID`,
+                                         `NAME`
+                                  FROM ZERN
+                                  WHERE {$where}
+                                  GROUP BY ID
+                                  ORDER BY {$order['by']} {$order['dir']} {$limit}
+        ");
+
+        $dataTotal = $this->DB->Query("SELECT count(*) val
+                                       FROM ZERN
+        ")->Fetch();
+
+        $dataFiltered = $this->DB->Query("SELECT count(*) val
+                                          FROM ZERN
+        ")->Fetch();
+
+        while ($row = $data->Fetch()) {
+            $row['material_name'] = $row['NAME'];
+            $result[] = $row;
+        }
+
+        $result['recordsTotal'] = $dataTotal['val'];
+        $result['recordsFiltered'] = $dataFiltered['val'];
+
+        return $result;
+    }
+}
