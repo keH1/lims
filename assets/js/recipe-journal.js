f36444e69248a0768e1a52b6e92bf92653251ae9
Родионов Роман <r.roman@niistrom.pro>
2025-03-21 15:52:41 +0500

    task/LOKI-1286/Добавил фильтр по объекту строительства

--- assets/js/recipe-journal.js
+++ assets/js/recipe-journal.js
@@ -93,12 +93,17 @@ $(function ($) {
         fixedHeader: false,
     })
 
-    recipeJournal.columns().every(function () {
-        $(this.header()).closest('thead').find('.search:eq(' + this.index() + ')').on('keyup change clear', function () {
-            recipeJournal
-                .column($(this).parent().index())
-                .search(this.value)
-                .draw()
+    recipeJournal.columns().every(function() {
+        let timeout
+        $(this.header()).closest('thead').find('.search:eq(' + this.index() + ')').on('keyup change clear', function() {
+            clearTimeout(timeout)
+            const searchValue = this.value
+            timeout = setTimeout(function() {
+                recipeJournal
+                    .column($(this).parent().index())
+                    .search(searchValue)
+                    .draw()
+            }.bind(this), 1000)
         })
     })
 
