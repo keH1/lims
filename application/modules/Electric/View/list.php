03b2710f3114229fa7007685221babe55b7be66e
Коваль Александр <a.koval@niistrom.pro>
2025-04-07 09:38:36 +0500

    hotfix/LOKI-2258. Убрал фиксацию заголовка при прокрутке, добавил кнопки для прокрутки влево и вправо.

--- application/modules/Electric/View/list.php
+++ application/modules/Electric/View/list.php
@@ -95,16 +95,16 @@
     </tbody>
 </table>
 
-<svg width="0" height="0" class="hidden">
-    <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="arrow-left">
-        <path
-            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-    </symbol>
-    <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="arrow-right">
-        <path
-            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-    </symbol>
-</svg>
+<div class='arrowLeft'>
+    <svg class="bi" width="40" height="40">
+        <use xlink:href="<?=URI?>/assets/images/icons.svg#arrow-left"/>
+    </svg>
+</div>
+<div class='arrowRight'>
+    <svg class="bi" width="40" height="40">
+        <use xlink:href="<?=URI?>/assets/images/icons.svg#arrow-right"/>
+    </svg>
+</div>
 
 <form id="add-entry-modal-form-first" class="bg-light mfp-hide col-md-4 m-auto p-3 position-relative"
       action="/ulab/electric/addMeasurement/" method="post">
