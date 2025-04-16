9cde3a2af5ee745622be2afbd2b32dc50b2f2be9
Коваль Александр <a.koval@niistrom.pro>
2025-04-16 19:42:15 +0500

    fix/LOKI-2694 В поле Основание для проведения испытаний добавил "Новый договор" по умолчанию.

--- assets/js/script.js
+++ assets/js/script.js
@@ -360,7 +360,7 @@ $(function ($) {
     $body.on('change', '#company', function() {
         // clear form
         let $selectContract = $('select[name="NUM_DOGOVOR"]')
-        $selectContract.empty().append(`<option value=""></option>`)
+        $selectContract.empty().append(`<option value="">Новый договор</option>`)
         $('input.clearable').val('')
 
         let text = $(this).val()
