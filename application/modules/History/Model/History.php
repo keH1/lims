4bcab54dc97ed5ee084be342473be59f0ed187e2
Родионов Роман <r.roman@niistrom.pro>
2025-04-17 17:20:30 +0500

    refactor/LOKI-3022 Рефакторинг журнала истории изменений

--- application/modules/History/Model/History.php
+++ application/modules/History/Model/History.php
@@ -7,5 +7,95 @@ class History extends Model
 		$sql = $this->prepearTableData('HISTORY', $data);
 		$this->DB->Insert('HISTORY', $sql);
 	}
-}
 
+	/**
+	 * @param array $filter
+	 * @return array
+	 */
+	public function getDataToJournalHistory(array $filter = []): array
+	{
+        $where = "";
+        $limit = "";
+        $order = [
+            'by' => 'DATE',
+            'dir' => 'DESC'
+        ];
+
+        if ( !empty($filter) ) {
+            // работа с фильтрами
+            if ( !empty($filter['search']) ) {
+				// Дата
+				if ( isset($filter['search']['dateStart']) ) {
+					$where .= "(DATE >= '{$filter['search']['dateStart']}' AND DATE <= '{$filter['search']['dateEnd']}') AND ";
+				}
+                if ( isset($filter['search']['REQUEST']) ) {
+                    $where .= "REQUEST LIKE '%{$filter['search']['REQUEST']}%' AND ";
+                }
+				if ( isset($filter['search']['PROT_NUM']) ) {
+                    $where .= "PROT_NUM = '{$filter['search']['PROT_NUM']}' AND ";
+                }
+				if ( isset($filter['search']['TZ_ID']) ) {
+                    $where .= "TZ_ID = '{$filter['search']['TZ_ID']}' AND ";
+                }
+				if ( isset($filter['search']['DATE']) ) {
+                    $where .= "DATE LIKE '%{$filter['search']['DATE']}%' AND ";
+                }
+				if ( isset($filter['search']['TYPE']) ) {
+                    $where .= "TYPE LIKE '%{$filter['search']['TYPE']}%' AND ";
+                }
+				if ( isset($filter['search']['ASSIGNED']) ) {
+                    $where .= "ASSIGNED LIKE '%{$filter['search']['ASSIGNED']}%' AND ";
+                }
+            }
+
+            // работа с сортировкой
+            if ( !empty($filter['order']) ) {
+                if ( $filter['order']['dir'] === 'asc' ) {
+                    $order['dir'] = 'ASC';
+                }
+            }
+
+            // работа с пагинацией
+            if ( isset($filter['paginate']) ) {
+                $offset = 0;
+                // количество строк на страницу
+                if ( isset($filter['paginate']['length']) && $filter['paginate']['length'] > 0 ) {
+                    $length = $filter['paginate']['length'];
+
+                    if ( isset($filter['paginate']['start']) && $filter['paginate']['start'] > 0 ) {
+                        $offset = $filter['paginate']['start'];
+                    }
+                    $limit = "LIMIT {$offset}, {$length}";
+                }
+            }
+        }
+        $where .= "1 ";
+
+        $result = [];
+
+        $data = $this->DB->Query("SELECT *
+                                  FROM HISTORY
+                                  WHERE {$where}
+                                  GROUP BY ID
+                                  ORDER BY {$order['by']} {$order['dir']} {$limit}
+        ");
+
+        $dataTotal = $this->DB->Query("SELECT count(*) val
+                                       FROM HISTORY
+        ")->Fetch();
+
+        $dataFiltered = $this->DB->Query("SELECT count(*) val
+                                          FROM HISTORY
+										  WHERE {$where}
+        ")->Fetch();
+
+        while ($row = $data->Fetch()) {
+            $result[] = $row;
+        }
+
+        $result['recordsTotal'] = $dataTotal['val'];
+        $result['recordsFiltered'] = $dataFiltered['val'];
+
+        return $result;
+    }
+}
---
+++ application/modules/History/View/list.php
@@ -0,0 +1,55 @@
+<div class="filters mb-4">
+    <div class="row">
+        <div class="col">
+            <input type="date" id="inputDateStart"
+                   class="form-control filter filter-date-start"
+                   value="<?=date('Y-m-d', strtotime('-1 year'))?>"
+                   placeholder="Введите дату начала:">
+        </div>
+
+        <div class="col">
+            <input type="date" id="inputDateEnd"
+                   class="form-control filter filter-date-end"
+                   value="<?=date('Y-m-d')?>" placeholder="Введите дату окончания:">
+        </div>
+
+        <div class="col-auto">
+            <button type="button" class="btn btn-outline-secondary filter-btn-reset">Сбросить</button>
+        </div>
+    </div>
+</div>
+
+<table id="journal_history" class="table table-striped journal">
+    <thead>
+        <tr class="table-light">
+            <th scope="col" class="text-nowrap">Заявка</th>
+            <th scope="col" class="text-nowrap">Протокол</th>
+            <th scope="col" class="text-nowrap">ТЗ</th>
+            <th scope="col" class="text-nowrap">Дата</th>
+            <th scope="col" class="text-nowrap">Тип изменений</th>
+            <th scope="col" class="text-nowrap">Пользователь</th>
+        </tr>
+        <tr class="header-search">
+            <th scope="col">
+                <input type="text" class="form-control search">
+            </th>
+            <th scope="col">
+                <input type="text" class="form-control search">
+            </th>
+            <th scope="col">
+                <input type="text" class="form-control search">
+            </th>
+            <th scope="col">
+                <input type="text" class="form-control search">
+            </th>
+            <th scope="col">
+                <input type="text" class="form-control search">
+            </th>
+            <th scope="col">
+                <input type="text" class="form-control search">
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
