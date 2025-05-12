ffef4c40b19ec13da1372be6d8f54a4b49ffc7d3
Лобанов Ярослав <1101z1111@gmail.com>
2025-05-12 13:28:55 +0500

    fix/LOKI-3524 исправил листы измерений по расчету среднего значения

--- application/modules/Result/View/template/average4.php
+++ application/modules/Result/View/template/average4.php
@@ -1,38 +1,38 @@
 <div class="measurement-wrapper" id="averageWrapper">
     <h3 class="mb-3">Методика с расчетом среднего значения по 4 наиб.</h3>
 
-    <!--name="method_type" наименавание для всех типов метода одинаково-->
+    <!--name="method_type" наименование для всех типов метода одинаково-->
     <!--<input type="hidden" id="methodType" name="method_type" value="TU_sred4">-->
     <input type="hidden" id="methodType" name="type" value="sred">
 
     <div class="form-group row">
         <div class="col-auto">
-            <input class="form-check-input border-radius-0" type="checkbox" value="1" name="is_single_values"
+            <input class="form-check-input border-radius-0" type="checkbox" value="1" name="form_data[<?=$this->data['ugtp_id']?>][is_single_values]"
                 <?=$this->data['measuring']['is_single_values'] == 1? 'checked' : ''?>>
         </div>
-        <label class="col col-form-label ps-0">Выводить единичные значения в протокол?</label>
+        <label class="col ps-0">Выводить единичные значения в протокол?</label>
     </div>
 
     <div class="mb-3">
         <label for="actualValue" class="form-label mb-1">Фактическое значение</label>
         <div class="td-actual-value">
             <input type="number" class="me-2 mb-2 actual-value w-100 border p-2 bg-white"
-                   name="actual_value[0]" step="any"
+                   name="form_data[<?=$this->data['ugtp_id']?>][actual_value][0]" step="any"
                    value="<?= $this->data['measuring']['actual_value'][0] ?>" required>
             <input type="number" class="me-2 mb-2 actual-value w-100 border p-2 bg-white"
-                   name="actual_value[1]" step="any"
+                   name="form_data[<?=$this->data['ugtp_id']?>][actual_value][1]" step="any"
                    value="<?= $this->data['measuring']['actual_value'][1] ?>" required>
             <input type="number" class="me-2 mb-2 actual-value w-100 border p-2 bg-white"
-                   name="actual_value[2]" step="any"
+                   name="form_data[<?=$this->data['ugtp_id']?>][actual_value][2]" step="any"
                    value="<?= $this->data['measuring']['actual_value'][2] ?>" required>
             <input type="number" class="me-2 mb-2 actual-value w-100 border p-2 bg-white"
-                   name="actual_value[3]" step="any"
+                   name="form_data[<?=$this->data['ugtp_id']?>][actual_value][3]" step="any"
                    value="<?= $this->data['measuring']['actual_value'][3] ?>" required>
             <input type="number" class="me-2 mb-2 actual-value w-100 border p-2 bg-white"
-                   name="actual_value[4]" step="any"
+                   name="form_data[<?=$this->data['ugtp_id']?>][actual_value][4]" step="any"
                    value="<?= $this->data['measuring']['actual_value'][4] ?>" required>
             <input type="number" class="me-2 actual-value w-100 border p-2 bg-white"
-                   name="actual_value[5]" step="any"
+                   name="form_data[<?=$this->data['ugtp_id']?>][actual_value][5]" step="any"
                    value="<?= $this->data['measuring']['actual_value'][5] ?>" required>
         </div>
     </div>
@@ -41,12 +41,12 @@
         <div class="col">
             <label for="averageValue" class="form-label mb-1">Среднее значение</label>
             <!--name="result_value" наименавание инпута результат которого хотим видеть в результатах испытаний "Фактическое значение" в поле для всех одинаково-->
-            <input type="number" class="me-2 w-100 border p-2 bg-white" id="averageValue" name="result_value"
+            <input type="number" class="me-2 w-100 border p-2 bg-white" id="averageValue" name="form_data[<?=$this->data['ugtp_id']?>][result_value]"
                    step="any" value="<?= $this->data['measuring']['result_value'] ?>">
         </div>
         <div class="col">
             <label for="decimalPlaces" class="form-label mb-1">Знаков после запятой</label>
-            <input type="number" class="me-2 w-100 border p-2 bg-white" id="decimalPlaces" name="decimal_places"
+            <input type="number" class="me-2 w-100 border p-2 bg-white" id="decimalPlaces" name="form_data[<?=$this->data['ugtp_id']?>][decimal_places]"
                    step="1" value="<?= $this->data['measuring_property']['decimal'] ?>">
         </div>
     </div>
